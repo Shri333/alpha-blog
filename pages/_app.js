2a472878232e26d1dc5ec925802aac9d2ba867c3
Shri333 <dshrihan@gmail.com>
2021-01-10 17:32:06 -0500

    im done

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,11 @@ import Head from "next/head";
 import Link from "next/link";
 import firebase from "firebase/app";
 import "firebase/auth";
+import { config } from "../config"
 import "../styles/global.css";
 
 if (!firebase.apps.length) {
-  firebase.initializeApp({
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID,
-  });
+  firebase.initializeApp(config);
 }
 
 function MyApp({ Component, pageProps }) {
