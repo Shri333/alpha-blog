310c22f8c0695b92e60ba8c0dfe25128ef903328
Shri333 <dshrihan@gmail.com>
2021-01-10 17:15:03 -0500

    env

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,18 @@ import Head from "next/head";
 import Link from "next/link";
 import firebase from "firebase/app";
 import "firebase/auth";
-import { firebaseConfig } from "../config";
 import "../styles/global.css";
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp({
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
+  });
 }
 
 function MyApp({ Component, pageProps }) {
