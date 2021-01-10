d08ba1898a8debe34bcf320bf36fc77c3154f778
Shri333 <dshrihan@gmail.com>
2021-01-10 17:04:40 -0500

    new

--- config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCebXg1aqG3Zwkii-KwgS6IeZBCpWJec2s",
-  authDomain: "alpha-blog-shri.firebaseapp.com",
-  projectId: "alpha-blog-shri",
-  storageBucket: "alpha-blog-shri.appspot.com",
-  messagingSenderId: "3686461785",
-  appId: "1:3686461785:web:ed8ae1c40b2aa11747dc48",
-  measurementId: "G-E1SF8DKMBE",
-}
